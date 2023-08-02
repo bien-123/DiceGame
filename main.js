7caa4a63e8162b331e816ea920582b0a2ce8f2f2
bien-123 <bienle160720@gmail.com>
2023-08-02 23:14:25 +0700

    Lập trình VueJs - 10 Ràng buộc Class bằng VueJs

--- main.js
+++ main.js
@@ -2,11 +2,29 @@ var vueIntance = new Vue ({
     el: '#app', //gọi đến id là app
     data: {
         firstName: 'Nguyen',
+        about: '',
+        textClass: 'active',
+        isActive: true,
+        isError: false
     },
     methods: {
         handleKeyUp(e) {
             this.firstName = e.target.value
             // console.log(e.target)
+        },
+        changeActive() {
+            this.isActive = !this.isActive;
+        },
+        changeError() {
+            this.isError = !this.isError;
         }
     },
+    computed: {
+        objClass: function() {
+            return {
+                active: this.active, // active là tên class, this.active là giá trị true hoặc false
+                error: this.error
+            }
+        }
+    }
 });

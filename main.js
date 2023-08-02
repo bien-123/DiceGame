f09808e62228816d74ce1609dc2e539c998210f1
bien-123 <bienle160720@gmail.com>
2023-08-03 02:59:14 +0700

    Lập trình VueJs - 12 Render Template dùng biểu thức điều kiện

--- main.js
+++ main.js
@@ -1,45 +1,14 @@
 var vueIntance = new Vue ({
     el: '#app', //gọi đến id là app
     data: {
-        firstName: 'Nguyen',
-        about: '',
-        textClass: 'active',
-        isActive: false,
-        isError: false,
-        activeColor: 'red',
-        fontSize: 30,
-        bg: 'https://img.freepik.com/free-photo/abstract-grunge-decorative-relief-navy-blue-stucco-wall-texture-wide-angle-rough-colored-background_1258-28311.jpg?w=2000'
+        tabSelected: 'login'
     },
     methods: {
-        handleKeyUp(e) {
-            this.firstName = e.target.value
-            // console.log(e.target)
-        },
-        changeActive() {
-            // true -> false
-            // false -> true
-            this.isActive = !this.isActive;
-        },
-        changeError() {
-            this.isError = !this.isError;
+        changeTabs(tab) {
+            this.tabSelected = tab;
         }
     },
     computed: {
-        objClass: function() {
-            return {
-                active: this.active,
-                error: this.error
-            }
-        },
-        background() {
-            return 'url(' + this.bg + ')';
-        },
-        objectStyle: function() {
-            return {
-                backgroundImage: 'url(' + this.bg + ')',
-                margin: '10px',
-                paddingTop: '10px'
-            }
-        }
+        
     }
 });

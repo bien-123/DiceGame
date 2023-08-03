81e6679b439890c20000f522db4ee78d7a7f377f
bien-123 <bienle160720@gmail.com>
2023-08-03 23:51:33 +0700

    Lập trình VueJs - 24 Tìm hiểu Component và cách tạo trong Vue

---
+++ src/components/CompFooter.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer><h1>{{text}}</h1></footer>
+</template>
+
+<script>
+export default {
+    name: "comp-footer",
+    data() {
+        return {
+            text: "Footer",
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
